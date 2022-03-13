9cfde1e3f0d33763c89939055b2a095fcfeb1177
Geisy <geisydomiciano@gmail.com>
2022-03-13 18:36:58 +0100

    :construction: integers and floats

---
+++ Data-types-and-operators/08-integers-and-float.py
@@ -0,0 +1,7 @@
+print(16/4) # any division results in a float
+
+print(int(10/3)) # won't round the number, only cut the decimal/floating part
+
+print(float(28)) #it will add 0 as decimal to the integer
+
+print(5/0) #generates exeption error: ZeroDivisionError
